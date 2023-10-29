a21a8d8c9b3eed182cc9d5bcf1cd670272f9663f
Doris940704 <146065147+Doris940704@users.noreply.github.com>
2023-10-29 23:47:17 +0800

    Update hw4-2.c

--- hw4-2.c
+++ hw4-2.c
@@ -6,26 +6,27 @@ main()//作業二
     int n;
     scanf("%d",&n);
     int ATM[n][2];
+    int m=0;
     for(int i=0;i<n;i++){
         for(int j=0;j<2;j++){
             scanf("%d",&ATM[i][j]);
 
         }
-        for(int k=0;k<6;k++){
-        if(ATM[i][0]==data_base[k][0]){
-            if(ATM[i][1]==data_base[k][1]){
-                printf("%d",data_base[k][2]);
-                break;
+    }
+        for(int i=0;i<6;i++){
+            for(int k=0;k<6;k++){
+                if(ATM[i][0]==data_base[k][0]&&ATM[i][1]==data_base[k][1]){
+                    printf("%d",data_base[k][2]);
+                    m=1;
+                    break;
+
             }
-            else{
-                printf("error");
-                break;
-            }
+
         }
-        else{
-            printf("error");
+        if(m!=1)
+            printf("error\n");
             break;
+
         }
-    }
-    }
-}
+return 0;
+            }
