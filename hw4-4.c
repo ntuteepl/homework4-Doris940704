af7e60901fb317cfdef02d371b4afb6d71502338
Doris940704 <146065147+Doris940704@users.noreply.github.com>
2023-10-29 23:09:07 +0800

    Update hw4-4.c

--- hw4-4.c
+++ hw4-4.c
@@ -1,1 +1,50 @@
+#include <stdlib.h>
+#include <stdio.h>
+main()
+{
+int num;
+scanf("%d",&num);
+if(num>=1&&num<=10){
+    int data[num][num];
+    for(int i=0;i<num;i++){
+        for(int j=0;j<num;j++){
+            scanf("%d",&data[i][j]);
+        }
+    }
+    int boy,girl,max;
+
+    max=0;
+    boy=0;
+    girl=0;
+    for(int i=0;i<num;i++){
+        for(int j=0;j<num;j++){
+        if(data[i][j]>max)
+            max=data[i][j];
+
+        }
+        for(int i=0;i<num;i++){
+            for(int j=0;j<num;j++){
+                if(data[i][j]==max){
+                boy=i;
+                girl=j;
+                printf("boy %d pair with girl %d\n",boy+1,girl+1);
+            }
+        }
+        }
+
+
+
+    }
+
+
+for(int i=0;i<num;i++){
+        for(int j=0;j<num;j++){
+            if(i==boy||j==girl)
+                data[i][j]=0;
+            }
+        }
+}
+
+}
+
 
